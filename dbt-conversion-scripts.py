7ef0b7c8c9c707d0e4382d5a0df71499a499d95e
Mahesh 🎨🌌 <47917543+mah35h95@users.noreply.github.com>
2024-01-14 22:42:52 +0530

    do a little extra 👾

--- dbt-conversion-scripts.py
+++ dbt-conversion-scripts.py
@@ -102,6 +102,22 @@ a - analytics
             return "hub"
 
 
+def getIngestLayerFromUser(table_name: str) -> str:
+    choice = input(
+        f"""Enter Ingest Layer for {table_name}:
+i - ingest stage (default value)
+d - datalake
+"""
+    )
+    match choice:
+        case "i":
+            return "i"
+        case "d":
+            return "d"
+        case _:
+            return "i"
+
+
 def findInListOfDict(list, key: str, value: str) -> int:
     for i, dic in enumerate(list):
         if dic[key] == value:
@@ -119,11 +135,6 @@ def readFile(filename: str) -> str:
     with open(filename) as f:
         data = f.read()
     return data
-
-
-def writeFile(filename: str, fileData: str):
-    with open(filename, "w") as f:
-        f.write(fileData)
 
 
 def readBetweenTheLine(fileContent: str, start: str, end: str) -> list[str]:
@@ -144,42 +155,259 @@ def isIngestStageSource(ref: str) -> bool:
     return tableCount > 0
 
 
+def getTableKeys(table_name: str) -> str:
+    table_keys_path = "table_keys.xlsx"
+    table_keys = pd.read_excel(table_keys_path, sheet_name="Sheet2")
+    return table_keys[table_keys["table"] == table_name.lower()].table_keys.item()
+
+
 def isTableInYML(filename: str, name: str, ref: str) -> bool:
     sourceFile = readYMLFile(filename)
     sourceIndex = findInListOfDict(sourceFile["sources"], "name", name)
+    if sourceIndex == -1:
+        return False
     tableIndex = findInListOfDict(
         sourceFile["sources"][sourceIndex]["tables"], "name", ref
     )
     return tableIndex != -1
 
 
-def addNewTableToSource(ref: str, filename: str, name: str):
-    newTable = {"name": ref}
+def getRandomPosition(listLength: int) -> int:
+    randIndex = 0
+    if listLength - 1 > 0:
+        randIndex = random.randrange(0, listLength - 1)
+    elif listLength == 1:
+        randIndex = 1
+    return randIndex
+
+
+def addNewTableToSourceYML(
+    tableToAppend: str,
+    filename: str,
+    name: str,
+    database: str,
+    schema: str,
+    warnCount: int,
+    warnPeriod: str,
+    errCount: int,
+    errPeriod: str,
+    loaded_at_field: str,
+):
+    newTable = {"name": tableToAppend}
     sourceFile = readYMLFile(filename)
     sourceIndex = findInListOfDict(sourceFile["sources"], "name", name)
+    if sourceIndex == -1:
+        sourceIndex = getRandomPosition(len(sourceFile["sources"]))
+        newSource = {
+            "name": name,
+            "database": database,
+            "schema": schema,
+            "freshness": {"warn_after": {"count": warnCount, "period": warnPeriod}},
+            "loaded_at_field": loaded_at_field,
+            "tables": [],
+        }
+        sourceFile["sources"].insert(sourceIndex, newSource.copy())
+
     sourceFile["sources"][sourceIndex]["tables"].insert(
-        random.randrange(0, len(sourceFile["sources"][sourceIndex]["tables"]) - 1),
+        getRandomPosition(len(sourceFile["sources"][sourceIndex]["tables"])),
         newTable.copy(),
     )
 
-    with open(filename, "w") as file:
-        yaml.dump(sourceFile, file, sort_keys=False, indent=4)
+    with open(filename, "w") as f:
+        yaml.dump(sourceFile, f, sort_keys=False, indent=4)
 
     ymlData = readFile(filename)
     ymlData = (
         ymlData.replace("version: 2", "version: 2\n")
         .replace("-  ", "  -")
+        .replace("'''", "'")
         .replace(
-            """freshness:
+            f"""freshness:
         warn_after:
-            count: 30
-            period: minute""",
-            """freshness: # default freshness
-      warn_after: { count: 30, period: minute }
-      #error_after: {count: 8, period: hour}""",
+            count: {warnCount}
+            period: {warnPeriod}""",
+            f"""freshness: # default freshness
+      warn_after: {{ count: {warnCount}, period: {warnPeriod} }}
+      #error_after: {{count: {errCount}, period: {errPeriod}}}""",
         )
     )
-    writeFile(filename, ymlData)
+    writeToFile(filename, ymlData)
+
+
+def addNewTableToModelYML(folderPath: str, tableName: str, columnName: str):
+    filename = f"{folderPath}/_models.yml"
+
+    unique_column_name = ""
+    if columnName == "":
+        unique_column_name = "<TODO: ADD Primary Keys>"
+    else:
+        unique_column_name = '"' + columnName.replace(", ", " || '-' || ") + '"'
+
+    newModel = {
+        "name": tableName,
+        "latest_version": 1,
+        "versions": [{"v": 1}],
+        "tests": [{"unique": {"column_name": unique_column_name}}],
+    }
+    modelFile = readYMLFile(filename)
+
+    modelFile["models"].insert(
+        getRandomPosition(len(modelFile["models"])), newModel.copy()
+    )
+
+    with open(filename, "w") as f:
+        yaml.dump(modelFile, f, sort_keys=False, indent=4)
+
+    ymlData = readFile(filename)
+    ymlData = ymlData.replace("version: 2", "version: 2\n").replace("-  ", "  -")
+    writeToFile(filename, ymlData)
+
+
+def createIngestSQLFiles(folderPath: str, ref: str, tableKeys: str):
+    partitionPKs = ""
+    unique_key: list[str] = []
+
+    if tableKeys == "":
+        partitionPKs = "<TODO: ADD Primary Keys>"
+        unique_key = ["<TODO: ADD Primary Keys>"]
+    else:
+        partitionPKs = tableKeys
+        unique_key = tableKeys.split(", ")
+
+    sqlData = f"""{{{{
+    config(
+        materialized            =   'incremental',
+        incremental_strategy    =   'merge',
+        incremental_predicates  =   ["dynamic_timestamp_range", "recordstamp"],
+        unique_key              =   [{"'" + "', '".join(unique_key) + "'"}],
+        cluster_by              =   ['recordstamp'],  
+        partition_by            =   {{
+                                    "field": "recordstamp",
+                                    "data_type": "timestamp",
+                                    "granularity": "day"
+                                }}
+    )
+}}}}
+
+select  
+    *,
+    current_timestamp() as metadata_inserted_timestamp,
+    current_timestamp() as metadata_updated_timestamp,  
+    case
+        when operation_flag = 'D' then 1
+        when operation_flag = 'U' then 2
+        when operation_flag = 'I' then 3
+    else 4
+    end as operation_rank
+from {{{{ source('ingest_stage_hana_s4_ppf', '{ref}') }}}}
+
+{{% if is_incremental() %}}
+    {{% set min_max_timestamp_query_result = get_incremental_timestamp('recordstamp', '120') %}} 
+    for SYSTEM_TIME as of TIMESTAMP_MILLIS(UNIX_MILLIS(TIMESTAMP '{{{{min_max_timestamp_query_result.max}}}}'))
+    -- this filter will only be applied on an incremental run
+    where recordstamp >= '{{{{ min_max_timestamp_query_result.min }}}}' and recordstamp < '{{{{min_max_timestamp_query_result.max}}}}'
+{{% endif %}}
+
+QUALIFY ROW_NUMBER() OVER (PARTITION BY {partitionPKs} ORDER BY recordstamp DESC, operation_rank ASC) = 1 
+"""
+    filename = f"{folderPath}/{ref}_current_v1.sql"
+    writeToFile(filename, sqlData)
+
+
+def createDatalakeSQLFiles(folderPath: str, ref: str, schemaName: str):
+    sqlData = f"""{{{{
+    config(
+        materialized            =   'incremental',
+        incremental_strategy    =   'merge',
+        incremental_predicates  =   ["dynamic_timestamp_range", "DICE_CHANGE_SOURCE_WATERMARK"],
+        unique_key              =   ['<TODO: ADD Primary Keys>'],
+        cluster_by              =   ['DICE_CHANGE_SOURCE_WATERMARK'],  
+        partition_by            =   {{
+                                    "field": "DICE_CHANGE_SOURCE_WATERMARK",
+                                    "data_type": "timestamp",
+                                    "granularity": "day"
+                                }}
+    )
+}}}}
+
+select  
+    *,
+    current_timestamp() as metadata_inserted_timestamp,
+    current_timestamp() as metadata_updated_timestamp,  
+    case
+        when DICE_CHANGE_INDICATOR = 'D' then 1
+        when DICE_CHANGE_INDICATOR = 'U' then 2
+        when DICE_CHANGE_INDICATOR = 'I' then 3
+    else 4
+    end as operation_rank
+from {{{{ source('{schemaName}', '{ref}_change_hist') }}}}
+
+{{% if is_incremental() %}}
+    {{% set min_max_timestamp_query_result = get_incremental_timestamp('DICE_CHANGE_SOURCE_WATERMARK', '120') %}} 
+    for SYSTEM_TIME as of TIMESTAMP_MILLIS(UNIX_MILLIS(TIMESTAMP '{{{{min_max_timestamp_query_result.max}}}}'))
+    -- this filter will only be applied on an incremental run
+    where DICE_CHANGE_SOURCE_WATERMARK >= '{{{{ min_max_timestamp_query_result.min }}}}' and DICE_CHANGE_SOURCE_WATERMARK < '{{{{min_max_timestamp_query_result.max}}}}'
+{{% endif %}}
+
+QUALIFY ROW_NUMBER() OVER (PARTITION BY <TODO: ADD Primary Keys> ORDER BY DICE_CHANGE_SOURCE_WATERMARK DESC, operation_rank ASC) = 1 
+"""
+    filename = f"{folderPath}/{ref}_current_v1.sql"
+    writeToFile(filename, sqlData)
+
+
+def createIngestRefFiles(ref: str, filename: str, name: str, tableKeys: str):
+    folderPath = f"{datT}/models/raw/hana_s4_ppf"
+    database = """'{{env_var("DBT_SOURCE_INGEST_STAGE_GCP_PROJECT")}}'"""
+    schema = "S4HANA"
+    warnCount = 30
+    warnPeriod = "minute"
+    errCount = 8
+    errPeriod = "hour"
+    loaded_at_field = "recordstamp"
+    addNewTableToSourceYML(
+        ref,
+        filename,
+        name,
+        database,
+        schema,
+        warnCount,
+        warnPeriod,
+        errCount,
+        errPeriod,
+        loaded_at_field,
+    )
+    tableName = f"{ref}_current"
+    addNewTableToModelYML(folderPath, tableName, tableKeys)
+    createIngestSQLFiles(folderPath, ref, tableKeys)
+
+
+def createDatalakeRefFiles(
+    ref: str, schemaName: str, filename: str, name: str, tableKeys: str
+):
+    folderPath = f"{datT}/models/raw/dice_sources"
+    database = """'{{env_var("DBT_SOURCE_LAKE_GCP_PROJECT")}}'"""
+    schema = schemaName
+    warnCount = 24
+    warnPeriod = "hour"
+    errCount = 48
+    errPeriod = "hour"
+    loaded_at_field = "dice_change_source_watermark"
+    changeHistName = f"{ref}_change_hist"
+    addNewTableToSourceYML(
+        changeHistName,
+        filename,
+        name,
+        database,
+        schema,
+        warnCount,
+        warnPeriod,
+        errCount,
+        errPeriod,
+        loaded_at_field,
+    )
+    tableName = f"{ref}_current"
+    addNewTableToModelYML(folderPath, tableName, tableKeys)
+    createDatalakeSQLFiles(folderPath, ref, schemaName)
 
 
 def createRefFiles(refs: list[str]):
@@ -189,29 +417,151 @@ def createRefFiles(refs: list[str]):
 
         if isStage(ref) or isHub(ref) or isAnalytics(ref):
             layer = getLayer(ref)
-            filePath = f"{datT}/models/raw/_sources/_{layer}_sources.yml"
-            # TODO: Write to respective layer yml file
+            filename = f"{datT}/models/raw/_sources/_{layer}_sources.yml"
+            database = (
+                f"""'{{{{env_var("DBT_SOURCE_{layer.upper()}_GCP_PROJECT")}}}}'"""
+            )
+            warnCount = 24
+            warnPeriod = "hour"
+            errCount = 48
+            errPeriod = "hour"
+            loaded_at_field = f"metadata_{layer}_row_insert_timestamp"
+
             if isStage(ref):
                 name = f"{layer}_stage"
-                # writeToSourceYML(filePath,name,database,schema,freshness,loaded_at_field,tableToAppend)
-            else:
-                name = layer
-                # writeToSourceYML(filePath,name,database,schema,freshness,loaded_at_field,tableToAppend)
-        else:
-            ref = removeDiceTableSuffix(ref)
-            if isIngestStageSource(ref):
-                filename = f"{datT}/models/raw/_sources/_ingest_stage_sources.yml"
-                folderPath = f"{datT}/models/raw/hana_s4_ppf/"
-                name = "ingest_stage_hana_s4_ppf"
-
+                schema = name
                 if isTableInYML(filename, name, ref):
                     print(f"{ref} already exists")
                 else:
-                    addNewTableToSource(ref, filename, name)
+                    addNewTableToSourceYML(
+                        ref,
+                        filename,
+                        name,
+                        database,
+                        schema,
+                        warnCount,
+                        warnPeriod,
+                        errCount,
+                        errPeriod,
+                        loaded_at_field,
+                    )
+            else:
+                name = layer
+                schema = name
+                if isTableInYML(filename, name, ref):
+                    print(f"{ref} already exists")
+                else:
+                    addNewTableToSourceYML(
+                        ref,
+                        filename,
+                        name,
+                        database,
+                        schema,
+                        warnCount,
+                        warnPeriod,
+                        errCount,
+                        errPeriod,
+                        loaded_at_field,
+                    )
+        else:
+            ref = removeDiceTableSuffix(ref)
+            filename = f"{datT}/models/raw/_sources/_ingest_stage_sources.yml"
+            name = "ingest_stage_hana_s4_ppf"
+            if isIngestStageSource(ref):
+                if isTableInYML(filename, name, ref):
+                    print(f"{ref} already exists")
+                else:
+                    tableKeys = getTableKeys(ref)
+                    createIngestRefFiles(ref, filename, name, tableKeys)
 
             else:
-                filePath = f"{datT}/models/raw/_sources/_ingest_stage_sources.yml"
-                folderPath = f"{datT}/models/raw/dice_sources/"
+                #! No Keys
+                choice = getIngestLayerFromUser(ref)
+                if choice == "i":
+                    if isTableInYML(filename, name, ref):
+                        print(f"{ref} already exists")
+                    else:
+                        createIngestRefFiles(ref, filename, name, "")
+                else:
+                    changeHistName = f"{ref}_change_hist"
+                    schemaName = input(f"Enter the schema name for {changeHistName}: ")
+                    filename = f"{datT}/models/raw/_sources/_lake_sources.yml"
+                    name = schemaName
+                    if isTableInYML(filename, name, changeHistName):
+                        print(f"{changeHistName} already exists")
+                    else:
+                        createDatalakeRefFiles(ref, schemaName, filename, name, "")
+
+
+def createSourceFiles(sources: list[str]):
+    setOfSources = set(sources)
+    for source in setOfSources:
+        source = source.replace("'", "")
+        sourceSplit = source.split(", ")
+        schemaName = sourceSplit[0]
+        ref = removeDiceTableSuffix(sourceSplit[1])
+        filename = f"{datT}/models/raw/_sources/_lake_sources.yml"
+        name = schemaName
+        changeHistName = f"{ref}_change_hist"
+        if isTableInYML(filename, name, changeHistName):
+            print(f"{changeHistName} already exists")
+        else:
+            createDatalakeRefFiles(ref, schemaName, filename, name, "")
+
+
+def convertSourcesToRefs(filename: str, sources: list[str]):
+    setOfSources = set(sources)
+    for source in setOfSources:
+        fromStr = f"{{{{ source({source}) }}}}"
+        sourceSplit = source.split(", ")
+        ref = sourceSplit[1]
+        toStr = f"{{{{ ref({ref}) }}}}"
+
+        sqlData = readFile(filename)
+        sqlData = sqlData.replace(fromStr, toStr)
+        writeToFile(filename, sqlData)
+
+
+def addIncrementalLine(filename: str):
+    fromStr = "-- this filter will only be applied on an incremental run"
+    toStr = """-- this filter will only be applied on an incremental run
+        where/and recordstamp/DICE_CHANGE_SOURCE_WATERMARK > '{{ min_max_cdc_timestamp.min }}' and recordstamp/DICE_CHANGE_SOURCE_WATERMARK <= '{{min_max_cdc_timestamp.max}}'
+"""
+    sqlData = readFile(filename)
+    sqlData = sqlData.replace(fromStr, toStr)
+    writeToFile(filename, sqlData)
+
+
+def addCdcColumns(filename: str):
+    fromStr = "current_timestamp() as metadata_inserted_timestamp"
+    toStr = """cdc_operation_type -- operation_flag/DICE_CHANGE_INDICATOR       as cdc_operation_type
+    ,       cdc_timestamp      -- recordstamp/DICE_CHANGE_SOURCE_WATERMARK   as cdc_timestamp
+    ,       current_timestamp() as metadata_inserted_timestamp"""
+    sqlData = readFile(filename)
+    sqlData = sqlData.replace(fromStr, toStr)
+    writeToFile(filename, sqlData)
+
+
+def addCdcOperationType(filename: str):
+    fromStr = """- name: metadata_inserted_timestamp
+        data_type: timestamp"""
+    toStr = """- name: cdc_operation_type
+        data_type: string
+      - name: metadata_inserted_timestamp
+        data_type: timestamp"""
+    ymlData = readFile(filename)
+    ymlData = ymlData.replace(fromStr, toStr)
+    writeToFile(filename, ymlData)
+
+
+def removeCdcTimestamp(filename: str):
+    fromStr = """- name: cdc_timestamp
+        data_type: timestamp
+      """
+    toStr = ""
+    ymlData = readFile(filename)
+    ymlData = ymlData.replace(fromStr, toStr)
+    writeToFile(filename, ymlData)
 
 
 def do_type0(table_name: str, layer: str):
@@ -318,11 +668,22 @@ git commit -m "Adding in files for {table_name} {emojis}"
         f"{datT}/models/staging/{layer}/ppf/stg_{table_name}/stg_{table_name}_v1.sql"
     )
     stgFile = readFile(stgPath)
+
     refs = readBetweenTheLine(stgFile, "{{ ref(", ") }}")
-    print(refs)
     createRefFiles(refs)
+
     sources = readBetweenTheLine(stgFile, "{{ source(", ") }}")
-    print(sources)
+    createSourceFiles(sources)
+    convertSourcesToRefs(stgPath, sources)
+
+    addIncrementalLine(stgPath)
+    addCdcColumns(stgPath)
+
+    stgModelPath = f"{datT}/models/staging/{layer}/ppf/stg_{table_name}/_models.yml"
+    addCdcOperationType(stgModelPath)
+
+    legacyModelPath = f"{datT}/models/marts/{layer}/ppf/legacy_{table_name}/_models.yml"
+    removeCdcTimestamp(legacyModelPath)
 
 
 # Processing start
